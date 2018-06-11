3f6e11fe9e10843697de623a2ec3db1751bfcc01
atomliu217 <46104531@qq.com>
2018-06-11 14:19:45 +0800

    my first
    Signed-off-by: atomliu217 <46104531@qq.com>

--- app/src/main/java/com/example/administrator/puzzle/MainActivity.java
+++ app/src/main/java/com/example/administrator/puzzle/MainActivity.java
@@ -2,12 +2,18 @@ package com.example.administrator.puzzle;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(1);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        getSupportActionBar().hide();
         setContentView(R.layout.activity_main);
     }
 }
